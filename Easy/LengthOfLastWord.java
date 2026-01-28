5a40178a3432d220400bc4ee23c0a08041db5146
Simran Mandal <simranmandal0211@gmail.com>
2026-01-28 19:14:53 +0530

    LengthOfLastWord :: string logic

---
+++ Easy/LengthOfLastWord.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        boolean foundChar = false;
+
+        for(int i = s.length() -1; i >= 0; i--){
+            char ch = s.charAt(i);
+
+            if(ch != ' '){
+                foundChar = true;
+                count++;
+            }else{
+                if(foundChar){
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
